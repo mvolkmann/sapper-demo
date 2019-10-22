cac77d4030ddb31d5ee413d364755b0f21582d5c
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2019-10-22 15:37:49 -0500

    added Docker files to test using it

---
+++ docker-start.sh
@@ -0,0 +1,11 @@
+#/usr/local/bin/bash
+# To run this, enter ". ./docker-start.sh"
+docker run \
+  --rm \
+  -u root \
+  -p 8080:8080 \
+  -v jenkins-data:/var/jenkins_home \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v "$HOME":/home \
+  jenkinsci/blueocean
+
